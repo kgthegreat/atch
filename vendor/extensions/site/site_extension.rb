dfd53e2eb51094d736b0181024a6fff65c853715
Kumar Gaurav <kgthegreat@gmail.com>
2010-10-15 21:23:00 +0530

    defaulted country, removed state, some seo, increased products per page to 12

--- vendor/extensions/site/site_extension.rb
+++ vendor/extensions/site/site_extension.rb
@@ -19,17 +19,20 @@ class SiteExtension < Spree::Extension
       preference :allow_ssl_in_production, :boolean, :default => false
       preference :default_locale, :string, :default => 'en-IN'
       preference :allow_locale_switching, :boolean, :default => false
-      preference :site_name, :string, :default => 'Quality groceries at your doorstep -- attachawal.com'
+      preference :site_name, :string, :default => 'Quality groceries at your doorstep -- attachawal.com -- Online Groceries in Bangalore, Best prices for groceries in bangalore, Door delivery of groceries in bangalore'
       preference :site_url, :string, :default => 'attachawal.com'
       preference :admin_interface_logo, :string, :default => '/images/attachawal_really_really_small.png'
       preference :default_country_id, :integer, :default => 92
       preference :address_require_state, :boolean, :default => false
+      preference :products_per_page, :integer, :default => 12
+      preference :address_requires_state, :boolean, :default => false
+      preference :checkout_zone, :string, :default => "India"
     #  preference :logo, :string, :default => '/images/attachawal_really_really_small.png'
      #  preference :allow_openid, false
     end
 
     Spree::Config.set(:logo =>  '/images/logo-test4.png')   # '/images/attachawal.png') # '/images/attachawal_really_really_small.png')
-    Spree::Config.set(:address_require_state => false)
+    #Spree::Config.set(:address_require_state => false)
 
     # make your helper avaliable in all views
     # Spree::BaseController.class_eval do
