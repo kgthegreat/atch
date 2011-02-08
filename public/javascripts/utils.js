8c3980a4912180143d0664f70d6aeb74f26b2fd0
Kumar Gaurav <kgthegreat@gmail.com>
2011-02-08 20:07:16 +0530

    changed $ to jQuery because of conflict with stepcarousel

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,11 +1,11 @@
 function update_listed_price(mrp,acp,id,element) {
 
+ // alert("aaya");
+    jQuery('.product-listing span#'+id+'acp').text('Our Price: Rs.' + acp);
+    jQuery('.product-listing span#'+id+'mrp').html('<del>MRP: Rs.'+mrp+'</del>');
 
-    $('.product-listing span#'+id+'acp').text('Our Price: Rs.' + acp);
-    $('.product-listing span#'+id+'mrp').html('<del>MRP: Rs.'+mrp+'</del>');
-    
-    ColourFade('.product-listing span#'+id+'mrp','#c9de55', 400, 400);
-    ColourFade('.product-listing span#'+id+'acp','#c9de55', 400, 400);
+    colourFade('.product-listing span#'+id+'mrp','#c9de55', 400, 400);
+    colourFade('.product-listing span#'+id+'acp','#c9de55', 400, 400);
     //ColourFade('.product-listing li#product_'+id +' div.product-title', 'Yellow');
 
 
@@ -15,12 +15,13 @@ function update_cart(name) {
     //alert(name);
     //setTimeout("ColourFade('#omnicart-list', '#c9de55')", 2600);
     //$.('#omnicart-list').html($.('#omnicart-list').html+'<li>'+ name + '</li>');
-    ColourFade('#omnicart-list', '#c9de55', 500,10000)
+    colourFade('#omnicart-list', '#c9de55', 500,10000);
 }
 
-function ColourFade(selector,color, time1, time2){
-    $(selector)
-	.css('opacity', 1)
-	.animate({ backgroundColor: color, opacity: 1.0 }, time1)
-	.animate({ backgroundColor: '#ffffff', opacity: 1.0}, time2 );
+function colourFade(selector,color, time1, time2){
+   // alert("aaya");
+    jQuery(selector)
+        .css('opacity', 1)
+        .animate({ backgroundColor: color, opacity: 1.0 }, time1)
+        .animate({ backgroundColor: '#ffffff', opacity: 1.0}, time2 );
 }
